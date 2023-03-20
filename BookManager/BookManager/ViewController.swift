817ab061ce415a2ee264b489cfb7241747cdcaf4
athata1 <akhil.thata@gmail.com>
2023-03-20 18:35:00 -0400

    Added boilerplate code

---
+++ BookManager/BookManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookManager
+//
+//  Created by Akhil Thata on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
