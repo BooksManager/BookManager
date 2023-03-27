d6b2eaebd5de1cc30c8a43828c8303f75ca37933
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-27 19:32:40 -0400

    BookView controller done
    - Can fetch data from firebase collection and display it in table view

--- BookManager/BookManager/BooksTableViewCell.swift
+++ BookManager/BookManager/BooksTableViewCell.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class BooksTableViewCell: UITableViewCell {
 
+ 
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var des: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
--- BookManager/BookManager/BooksViewController.swift
+++ BookManager/BookManager/BooksViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-import FirebaseDatabase
+import FirebaseFirestore
 
 class BooksViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var BooksTableView: UITableView!
-    private let db = Database.database().reference()
-    var books = [[String: Any]]()
+    private let db = Firestore.firestore()
+    var books = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,31 +19,43 @@ class BooksViewController: UIViewController,UITableViewDataSource, UITableViewDe
 
         BooksTableView.dataSource = self
         BooksTableView.delegate = self
-
+        self.BooksTableView
+            .rowHeight = 100
+        
+        let ref = db.collection("Users").document("1TkF9w3QD0lp48IudQnM").collection("Books")
+        let res = ref.getDocuments { (querySnapshot: QuerySnapshot?, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                
+                guard let results = querySnapshot else{
+                    return
+                }
+                for document in results.documents{
+                    self.books.append(document.data())
+                    self.BooksTableView.reloadData()
+                }
+            }
+        }
 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books.count
     }
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! BooksTableViewCell
-//        let book = books[indexPath.row]
-//        let title = books["original_title"] as! String
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BooksTableViewCell") as! BooksTableViewCell
+        let book = books[indexPath.row]
+        let title = book["Title"] as! String
+        let des = book["Description"] as! String
 //        let description = movie["overview"] as! String
-
-//        cell.BookTitle.text = title
-//        cell.BookTitle.sizeToFit()
-//        cell.BookDescription.text = description
-//        cell.BookDescription.sizeToFit()
-        
-  
-
-//        let baseURL = "https://image.tmdb.org/t/p/w185"
-//        let posterPath = movie["poster_path"] as! String
-//        let posterURL = URL(string: baseURL + posterPath)
-//        Nuke.loadImage(with:posterURL!, into:cell.PosterImage)
+//
+        cell.title.text = title
+        cell.title.sizeToFit()
+        cell.des.text = des
+        cell.des.sizeToFit()
 
         return cell
     }
@@ -58,5 +70,19 @@ class BooksViewController: UIViewController,UITableViewDataSource, UITableViewDe
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+
+        let cell = sender as! UITableViewCell
+        let indexPath = BooksTableView.indexPath(for: cell)!
+        let book = books[indexPath.row]
+    
+//        let detailViewController = segue.destination as! MovieDetailViewController
+//        detailViewController.movie = movie
+    
+        BooksTableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
