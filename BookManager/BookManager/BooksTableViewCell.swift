8ea01cf40bbb8cc30bdbe136d7894d158934b783
AudenHuang <35862858+AudenHuang@users.noreply.github.com>
2023-03-27 16:01:47 -0400

    added tableView into books View controller

---
+++ BookManager/BookManager/BooksTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BooksTableViewCell.swift
+//  BookManager
+//
+//  Created by Auden Huang on 3/27/23.
+//
+
+import UIKit
+
+class BooksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BookManager/BookManager/BooksViewController.swift
+++ BookManager/BookManager/BooksViewController.swift
@@ -6,13 +6,46 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class BooksViewController: UIViewController {
-
+class BooksViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var BooksTableView: UITableView!
+    private let db = Database.database().reference()
+    var books = [[String: Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
 
-        // Do any additional setup after loading the view.
+        BooksTableView.dataSource = self
+        BooksTableView.delegate = self
+
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! BooksTableViewCell
+//        let book = books[indexPath.row]
+//        let title = books["original_title"] as! String
+//        let description = movie["overview"] as! String
+
+//        cell.BookTitle.text = title
+//        cell.BookTitle.sizeToFit()
+//        cell.BookDescription.text = description
+//        cell.BookDescription.sizeToFit()
+        
+  
+
+//        let baseURL = "https://image.tmdb.org/t/p/w185"
+//        let posterPath = movie["poster_path"] as! String
+//        let posterURL = URL(string: baseURL + posterPath)
+//        Nuke.loadImage(with:posterURL!, into:cell.PosterImage)
+
+        return cell
     }
     
 
